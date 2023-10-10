c74aeabc3599b46ec27df29041ce6a11db67999e
ALEXANDR19835 <137157008+ALEXANDR19835@users.noreply.github.com>
2023-10-10 16:16:19 +0300

    ready

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,37 @@
+package web.model;
+
+public class Car {
+    private String model;
+    private String color;
+    private double price;
+
+    public Car(String model, String color, double price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
